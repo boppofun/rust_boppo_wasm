b2c8058046c8e6c2976c13091f2f2cf8a15efbb7
John Tiesselune <juliensos10@gmail.com>
2026-04-09 17:28:53 +0200

    WIP : WASM hal for lights

---
+++ src/hal/lights.rs
@@ -0,0 +1,23 @@
+use boppo_core::color::RGB;
+use boppo_core::hal::HalLights;
+
+unsafe extern "C" {
+    fn boppo_wasm_set_color(idx: i32, r: i32, g: i32, b: i32);
+    fn boppo_wasm_flush();
+}
+
+pub struct WasmHalLights;
+
+impl HalLights for WasmHalLights {
+    fn set_color(&mut self, idx: usize, color: RGB) {
+        unsafe {
+            boppo_wasm_set_color(idx as i32, color.r as i32, color.g as i32, color.b as i32);
+        }
+    }
+
+    fn flush(&mut self) {
+        unsafe {
+            boppo_wasm_flush();
+        }
+    }
+}
