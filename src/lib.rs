f484821eedbc40a98da25a78f5683150c5b0d8cd
John Tiesselune <juliensos10@gmail.com>
2026-04-28 10:31:30 +0200

    Avoided host event starvation by allowing polling with 0 timeout Updated comments about safety and replaced error logging with debug logging

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ mod host_ffi;
 pub mod logger;
 pub mod timer;
 
-pub use executor::block_on;
+pub use executor::internal_block_on;
 pub use executor::spawn;
 pub use host_ffi::host_event::HostEvent;
 
