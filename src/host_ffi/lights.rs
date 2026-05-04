440744db4b7d7b60a8f1e32e6cea04dad0ec073b
Ben Harris <benharrisdevelopment@gmail.com>
2026-05-04 17:33:49 +0100

    Use Mutex in WasmHalLights, implement set_all_colors, use cast fn and borrow as raw ptr instead of as-casts.

--- src/host_ffi/lights.rs
+++ src/host_ffi/lights.rs
@@ -1,7 +1,7 @@
 use std::os::raw::c_void;
+use std::sync::Mutex;
 
 use boppo_core::color::RGB;
-use boppo_core::color::rgb::Rgb;
 use boppo_core::hal::HalLights;
 use boppo_core::{Framebuffer, Lights, NUM_LIGHTS};
 
@@ -12,26 +12,32 @@ unsafe extern "C" {
 }
 
 pub struct WasmHalLights {
-    framebuffer: Framebuffer,
+    framebuffer: Mutex<Framebuffer>,
 }
 
 impl Default for WasmHalLights {
     fn default() -> Self {
         Self {
-            framebuffer: Framebuffer::new(),
+            framebuffer: Mutex::new(Framebuffer::new()),
         }
     }
 }
 
 impl HalLights for WasmHalLights {
-    fn set_color(&mut self, idx: usize, color: RGB) {
-        self.framebuffer.set_color(Lights::from_index(idx), color);
+    fn set_color(&self, idx: usize, color: RGB) {
+        let mut fb = self.framebuffer.lock().unwrap();
+        fb.set_color(Lights::from_index(idx), color);
     }
 
-    fn flush(&mut self) {
+    fn set_all_colors(&self, colors: &[boppo_core::color::RGB; NUM_LIGHTS]) {
+        let mut fb = self.framebuffer.lock().unwrap();
+        fb.set_all_colors(colors);
+    }
+
+    fn flush(&self) {
         unsafe {
             boppo_wasm_set_and_flush_lights(
-                &self.framebuffer.colors as *const [Rgb<u8>; NUM_LIGHTS] as *const c_void,
+                (&raw const self.framebuffer.lock().unwrap().colors).cast::<c_void>(),
             );
         }
     }
