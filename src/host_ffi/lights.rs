38090c48f221a68f529bf0fae971b90423e33d5c
Ben Hansen <git@benhansen.io>
2026-05-05 18:12:55 +0800

    update WasmHalLights for new HalLights API

--- src/host_ffi/lights.rs
+++ src/host_ffi/lights.rs
@@ -1,3 +1,4 @@
+use std::cell::RefCell;
 use std::os::raw::c_void;
 
 use boppo_core::color::RGB;
@@ -12,26 +13,33 @@ unsafe extern "C" {
 }
 
 pub struct WasmHalLights {
-    framebuffer: Framebuffer,
+    framebuffer: RefCell<Framebuffer>,
 }
 
 impl Default for WasmHalLights {
     fn default() -> Self {
         Self {
-            framebuffer: Framebuffer::new(),
+            framebuffer: RefCell::new(Framebuffer::new()),
         }
     }
 }
 
 impl HalLights for WasmHalLights {
-    fn set_color(&mut self, idx: usize, color: RGB) {
-        self.framebuffer.set_color(Lights::from_index(idx), color);
+    fn set_color(&self, idx: usize, color: RGB) {
+        self.framebuffer
+            .borrow_mut()
+            .set_color(Lights::from_index(idx), color);
     }
 
-    fn flush(&mut self) {
+    fn set_all_colors(&self, colors: &[boppo_core::color::RGB; NUM_LIGHTS]) {
+        self.framebuffer.borrow_mut().set_all_colors(colors);
+    }
+
+    fn flush(&self) {
+        // TODO lets pass in the lights here so we don't have to store them in framebuffer
         unsafe {
             boppo_wasm_set_and_flush_lights(
-                &self.framebuffer.colors as *const [Rgb<u8>; NUM_LIGHTS] as *const c_void,
+                &self.framebuffer.borrow().colors as *const [Rgb<u8>; NUM_LIGHTS] as *const c_void,
             );
         }
     }
