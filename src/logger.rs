9dad7f7131629ab477b8ca988f68bca07585a8df
Ben Hansen <git@benhansen.io>
2026-05-08 17:59:48 +0800

    shorten logging

--- src/logger.rs
+++ src/logger.rs
@@ -8,7 +8,14 @@ impl log::Log for WasmLogger {
     }
 
     fn log(&self, record: &log::Record) {
-        eprintln!("[{}] WASM Activity : {}", record.level(), record.args());
+        let level = match record.level() {
+            log::Level::Error => "E",
+            log::Level::Warn => "W",
+            log::Level::Info => "I",
+            log::Level::Debug => "D",
+            log::Level::Trace => "T",
+        };
+        eprintln!("{} wasm: {}", level, record.args());
     }
 
     fn flush(&self) {}
