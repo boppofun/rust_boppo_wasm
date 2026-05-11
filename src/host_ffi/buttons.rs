e4a6351c305204428111f2af86837e5ab24e84ca
Ben Hansen <git@benhansen.io>
2026-05-11 18:09:39 +0800

    also update BUTTON_COUNTS as well as send ButtonEvents

--- src/host_ffi/buttons.rs
+++ src/host_ffi/buttons.rs
@@ -1,17 +1,25 @@
 use std::sync::OnceLock;
 
 use boppo_core::ButtonEvent;
-use tokio::sync::broadcast;
+use boppo_core::hal::ButtonCounts;
+use tokio::sync::{broadcast, watch};
 
 static BUTTON_SENDER: OnceLock<broadcast::Sender<ButtonEvent>> = OnceLock::new();
+static BUTTON_COUNTS_SENDER: OnceLock<watch::Sender<ButtonCounts>> = OnceLock::new();
 
 ///Broadcasts a button event to all listeners registered through boppo_core's HAL, on the wasm side.
 pub fn broadcast_event(event: ButtonEvent) {
     let _ = BUTTON_SENDER.get().unwrap().send(event);
+    BUTTON_COUNTS_SENDER.get().unwrap().send_modify(|counts| {
+        counts.update_for_event(event.button(), event.is_pressed());
+    });
 }
 
 pub fn init() {
     let (sender, _) = broadcast::channel::<ButtonEvent>(16);
-    BUTTON_SENDER.set(sender.clone()).ok();
+    BUTTON_SENDER.set(sender.clone()).unwrap();
     boppo_core::hal::set_button_events(sender);
+    let (button_counts_sender, button_counts_receiver) = watch::channel(ButtonCounts::default());
+    BUTTON_COUNTS_SENDER.set(button_counts_sender).unwrap();
+    boppo_core::hal::set_button_counts(button_counts_receiver);
 }
