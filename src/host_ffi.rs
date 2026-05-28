14c1b6b3f8640a6276734a943058d5666f281650
John Tiesselune <juliensos10@gmail.com>
2026-05-28 15:10:26 +0200

    ✨ Ready notifier for test purposes

--- src/host_ffi.rs
+++ src/host_ffi.rs
@@ -8,8 +8,17 @@ pub mod host_event;
 pub mod lights;
 
 #[cfg(feature = "wasm_client")]
+#[link(wasm_import_module = "host")]
+unsafe extern "C" {
+    /// Notifies that the wasm file is ready to start
+    /// Used in test contexts.
+    pub fn boppo_ready();
+}
+
+#[cfg(feature = "wasm_client")]
 pub fn init() {
     lights::init_lights();
     buttons::init();
     audio::init();
+    unsafe { boppo_ready() };
 }
