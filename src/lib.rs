65285dca48e79e1bc34e0b2161b208e4c04d6031
Ben Hansen <git@benhansen.io>
2026-06-01 12:14:45 +0800

    init_and_run_async: reset audio and lights and micro sleep

--- src/lib.rs
+++ src/lib.rs
@@ -11,8 +11,10 @@ pub use error::Error;
 
 /// Initializes the Boppo WASM runtime and runs an async activity function.
 ///
-/// If the async function returns, it is called again, as most Boppo activities
-/// are expected to restart.
+/// If `activity_fn` returns, it is called again, as most Boppo activities
+/// are expected to start fresh after completion. All audio is stopped and the
+/// lights are turned off before starting again. The activity is passed in the
+/// number of times it has been started (the first time is 1).
 ///
 /// If you would like to return to the main menu, you can call std::process::exit(0).
 ///
@@ -23,17 +25,25 @@ pub use error::Error;
 ///     boppo_wasm::init_and_run_async(activity)
 /// }
 ///
-/// pub async fn activity() {
+/// pub async fn activity(num_starts: u32) {
 ///     Button::B0.set_color(color::BLUE);
 ///    // ...
 /// }
 /// ```
 #[cfg(feature = "wasm_client")]
-pub fn init_and_run_async(mut activity_fn: impl AsyncFnMut()) {
+pub fn init_and_run_async(mut activity_fn: impl AsyncFnMut(u32)) {
     internal::init();
     internal::block_on(async {
+        let mut num_starts = 1;
         loop {
-            activity_fn().await
+            activity_fn(num_starts).await;
+            audio::stop_all();
+            Buttons::all().set_color(color::OFF);
+            // Let's sleep a tiny bit in case there is an activity bug that
+            // causes an activity to end immediately so there isn't a CPU
+            // blocking loop.
+            boppo_core::executor::sleep_ms(50).await;
+            num_starts += 1;
         }
     })
 }
