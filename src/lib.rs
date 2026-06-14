af64f4248db8da00af2606a910bd58f33cfa85cd
Ben Hansen <git@benhansen.io>
2026-06-14 13:03:11 +0800

    docs: move README.md mostly back into lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,50 @@
-#![doc = include_str!("../README.md")]
+//! # [Boppo](https://developer.boppo.com) WebAssembly Activity API for Rust
+//!
+//! Activities are compiled to WASM and run on the tablet.
+//!
+//! ## Getting Started
+//!
+//! To get started developing on Boppo read our
+//! [developer documentation](https://developer.boppo.com/) with instructions
+//! to clone our template repository.
+//!
+//! The main entry point is [`init_and_run_async`].
+//!
+//! ## Lights
+//!
+//! For setting the color of lights see the set_color and similar functions on
+//! [`Button`], [`Buttons`], and [`Lights`].
+//!
+//! By default all changes are immediately flushed to the hardware. If you want
+//! to make multiple changes in a row and performance matters you can use
+//! [`Framebuffer`] or modify the auto flush behavior using [`MainFramebuffer`].
+//!
+//! Each Boppo button has 4 LED lights which are represented by [`LightDir`].
+//!
+//! For drawing and animating simple shapes treating the entire Boppo surface as
+//! a display see [`lights_plane`].
+//!
+//! ## Button Events
+//!
+//! You can receive button change events as an async stream using
+//! [`ButtonEvents`].
+//!
+//! You can also query the current state of the buttons using
+//! [`Button::is_pressed`] and [`Buttons::currently_pressed`].
+//!
+//! You can use [`Button::wait_for_press`] and [`Button::wait_for_release`] to
+//! wait for a button to be in a specific state.
+//!
+//! ## Audio
+//!
+//! Play audio using [`audio::play`] or the `audio::play_*` helper functions.
+//!
+//! ## Guidelines
+//!
+//! See Boppo's [Activity
+//! Guidelines](https://developer.boppo.com/docs/activity-guidelines) for
+//! guidelines on creating great activities.uch of the API (e.g. handling button
+//! events, setting button colors)
 #![deny(missing_docs)]
 
 pub mod audio;
