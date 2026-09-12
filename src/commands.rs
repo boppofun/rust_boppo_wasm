c476001d929260e1e7a5b144f4e398a4329952a8
Ben Harris <benharrisdevelopment@gmail.com>
2026-09-12 10:03:57 +0100

    Add ffi, wasm command for execute_command_with_writer

--- src/commands.rs
+++ src/commands.rs
@@ -25,3 +25,29 @@ pub fn execute_command(command: &str) -> Result<(), Error> {
     })?;
     Ok(())
 }
+
+/// Execute a command, outputting to `buf` instead of stdout.
+///
+/// See the full list of supported commands at [developer.boppo.com/docs/commands](https://developer.boppo.com/docs/commands)
+///
+/// ## Examples
+///
+/// ```no_run
+/// use boppo_wasm::commands::execute_command_with_buffer;
+///
+/// let mut buf = [0u8; 16];
+///
+/// // retreive the high score for an activity, and store its string in `buf`
+/// execute_command_with_buffer("score_retreive ...", &mut buf).unwrap();
+/// ```
+pub fn execute_command_with_buffer(command: &str, buffer: &mut [u8]) -> Result<(), Error> {
+    Error::result_from_i32(unsafe {
+        crate::internal::host_ffi::boppo_execute_command_with_buffer(
+            command.as_ptr(),
+            command.len(),
+            buffer.as_mut_ptr(),
+            buffer.len(),
+        )
+    })?;
+    Ok(())
+}
